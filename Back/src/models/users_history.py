4e108bd7075642c657673db7a5b389f0b20db31a
Maniko <carotte317@gmail.com>
2024-09-29 14:29:58 +0200

    refactor(Back): Change history timestamp type

--- Back/src/models/users_history.py
+++ Back/src/models/users_history.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
+import datetime
 
 class UserContentHistory(BaseModel):
     user_id: int
     content_id: int
     action: int
-    timestamp: str
+    timestamp: datetime.datetime
 
 class UserAlbumHistory(BaseModel):
     user_id: int
     album_id: int
     action: int
-    timestamp: str
+    timestamp: datetime.datetime
