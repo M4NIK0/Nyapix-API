fa69fb237a94e263ab35c0984e2124f0a49f1090
Maniko <carotte317@gmail.com>
2024-11-23 15:26:27 +0100

    feat(Back): Add user register

--- Back/src/db_management/users.py
+++ Back/src/db_management/users.py
@@ -1,12 +1,41 @@
 import utility.users as users_utility
+from typing import Union
+from utility.logging import logger
 
 def register(db, username: str, nickname: str, password: str) -> bool:
     """Register a new user in the database, returns True if the user was registered successfully, False otherwise"""
     cursor = db.cursor()
-    cursor.execute(
-        "INSERT INTO nyapixuser (username, nickname, password, user_type) VALUES (%s, %s, %s, %s)",
-        (username, nickname, password, users_utility.USER_TYPE.GUEST)
-    )
-    db.commit()
-    cursor.close()
+    try:
+        cursor.execute(
+            "INSERT INTO nyapixuser (username, nickname, password, user_type) VALUES (%s, %s, %s, %s)",
+            (username, nickname, password, users_utility.USER_TYPE.GUEST)
+        )
+        db.commit()
+    except Exception as e:
+        logger.error(e)
+        return False
+    finally:
+        cursor.close()
     return True
+
+def check_user_exists(db, user: Union[str, int]) -> bool:
+    """Check if a user with the given username exists in the database, returns True if the user exists, False otherwise"""
+    cursor = db.cursor()
+    try:
+        if type(user) == str:
+            cursor.execute("SELECT * FROM nyapixuser WHERE username = %s", (user,))
+            result = cursor.fetchone()
+            cursor.close()
+            return result is not None
+        elif type(user) == int:
+            cursor.execute("SELECT * FROM nyapixuser WHERE id = %s", (user,))
+            result = cursor.fetchone()
+            cursor.close()
+            return result is not None
+        else:
+            return False
+    except Exception as e:
+        logger.error(e)
+        return False
+    finally:
+        cursor.close()
--- Back/src/endpoints/login.py
+++ Back/src/endpoints/login.py
@@ -3,15 +3,19 @@ import utility.token as token_utility
 from utility.logging import logger
 from db_management.connection import connect_db
 import db_management.users as users_db
+import models.users as users_models
 
 router = fastapi.APIRouter()
 
 @router.post("/register")
-async def post_register_endpoint():
+async def post_register_endpoint(new_user: users_models.UserRegisterModel):
     db = None
     try:
         db = connect_db()
-        result = users_db.register(db, "test", "test", "test")
+        exists = users_db.check_user_exists(db, new_user.username)
+        if exists:
+            return fastapi.responses.Response(status_code=409)
+        result = users_db.register(db, new_user.username, new_user.nickname, new_user.password)
         if result is None:
             return fastapi.responses.Response(status_code=409)
     except Exception as e:
