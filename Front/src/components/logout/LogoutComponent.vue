968011df1bdaa118acbc1fa74918049962f4751a
Eth22-Epitech <ethan.flak@epitech.eu>
2024-12-27 17:11:59 +0100

    feat(Front): Implement NavBar and Logout

--- Front/src/components/logout/LogoutComponent.vue
+++ Front/src/components/logout/LogoutComponent.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { onMounted } from 'vue';
 
 const router = useRouter();
 
@@ -7,14 +8,12 @@ const logout = () => {
   localStorage.removeItem('token');
   router.push('/login');
 };
+
+onMounted(() => {
+  logout();
+});
 </script>
 
 <template>
-  <div>
-    <button
-      @click="logout"
-      class="bg-blue-500 text-white px-4 py-2 rounded transition transform hover:scale-105">
-      {{ 'Logout' }}
-    </button>
-  </div>
+  <div></div>
 </template>
