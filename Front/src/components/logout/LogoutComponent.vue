b97b9a56d02a8de916e4b808771ded2c74bfc843
Eth22-Epitech <ethan.flak@epitech.eu>
2024-12-30 14:42:33 +0100

    feat(Front): Implement deletion and editing content

--- Front/src/components/logout/LogoutComponent.vue
+++ Front/src/components/logout/LogoutComponent.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import { onMounted } from 'vue';
+import axios from 'axios';
 
 const router = useRouter();
 
-const logout = () => {
+const logout = async () => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    try {
+      await axios.delete('http://localhost:5000/v1/logout', {
+        headers: {
+          'accept': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  }
   localStorage.removeItem('token');
   router.push('/login');
 };
